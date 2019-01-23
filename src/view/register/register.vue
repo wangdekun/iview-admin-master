b4af1655a4bcb0bd54c1352b5736f1fac3cfdc5f
wangdekun <790649280@qq.com>
2019-01-22 23:22:33 -0500

    qqq

---
+++ src/view/register/register.vue
@@ -0,0 +1,48 @@
+<style lang="less">
+  @import './register.less';
+</style>
+
+<template>
+  <Row>
+        <Col span="12">
+            <DatePicker type="date" placeholder="Select date" style="width: 200px"></DatePicker>
+        </Col>
+        <Col span="12">
+            <DatePicker type="daterange" placement="bottom-end" placeholder="Select date" style="width: 200px"></DatePicker>
+        </Col>
+    </Row>
+</template>
+
+<script>
+import LoginForm from '_c/login-form'
+export default {
+  components: {
+    LoginForm
+  },
+  methods: {
+    handleSubmit ({ userName, password }) {
+      // let data = { userName, password }
+      this.$post(
+        '/test/setRedis',
+        {
+          // account: data.userName,
+          // password: data.password
+        },
+        response => {
+          console.log(response)
+          debugger
+          // if (response.data.code === '200') {
+          //   this.$router.push({ name: this.$config.homeName })
+          // } else {
+          //   this.$Message.error('账号或密码')
+          // }
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
