53c1d3837c72e89903e2eb0b2900ab1745ff8a59
wangdekun <790649280@qq.com>
2019-03-25 13:41:31 +0800

    导航

--- src/view/indexmenu/indexmenu.vue
+++ src/view/indexmenu/indexmenu.vue
@@ -1,39 +1,58 @@
-<style lang="less">
-  @import './indexmenu.less';
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 200px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
+        background: #464c5b;
+    }
+    .layout-header{
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+    }
 </style>
-
 <template>
     <div class="layout">
         <Row type="flex">
             <i-col span="5" class="layout-menu-left">
-                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-                    <div class="layout-logo-left"></div>
-                    <Submenu name="1">
-                        <template slot="title">
-                            <Icon type="ios-navigate"></Icon>
-                            导航一
-                        </template>
-                        <Menu-item name="1-1">选项 1</Menu-item>
-                        <Menu-item name="1-2">选项 2</Menu-item>
-                        <Menu-item name="1-3">选项 3</Menu-item>
-                    </Submenu>
-                    <Submenu name="2">
-                        <template slot="title">
-                            <Icon type="ios-keypad"></Icon>
-                            导航二
-                        </template>
-                        <Menu-item name="2-1">选项 1</Menu-item>
-                        <Menu-item name="2-2">选项 2</Menu-item>
-                    </Submenu>
-                    <Submenu name="3">
-                        <template slot="title">
-                            <Icon type="ios-analytics"></Icon>
-                            导航三
-                        </template>
-                        <Menu-item name="3-1">选项 1</Menu-item>
-                        <Menu-item name="3-2">选项 2</Menu-item>
-                    </Submenu>
-                </Menu>
+                <!-- <Menu mode="horizontal" theme="light" active-name="首页导航">
+                  <Submenu v-for="(setting_item, i) in website_settings_config_data" :name="setting_item.name">
+                    <template slot="title">
+                      <Icon :type="setting_item.icon"></Icon>
+                      {{ setting_item.name }}
+                    </template>
+                    <MenuGroup v-for="(group, j) in setting_item.groups" :title="group.name">
+                      <MenuItem v-for="(child, k) in group.children" name="child.name">{{child.name}}</MenuItem>
+                    </MenuGroup>
+                  </Submenu>
+                </Menu> -->
             </i-col>
             <i-col span="19">
                 <div class="layout-header"></div>
@@ -54,11 +73,164 @@
         </Row>
     </div>
 </template>
-
 <script>
 // import LoginForm from '_c/login-form'
 // import { setToken } from '@/libs/util'
-export default {
+// axios
+import axios from 'axios'
+// axios方法绑定到全局
+// Vue.prototype.$http = axios;
+axios.post('/tree/findtree.json', {
+})
+  .then(function (response) {
+    console.log(response)
+    var data = response.data
+    for (var i = 0; i < data.length; i++) {
+      state.menuRspList.push(list[i])
+    }
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+export default{
+  data () {
+    return {
+      website_settings_config_data: [
+        {
+          'name': '网站首页设置',
+          'icon': 'settings',
+          'groups': [
+            {
+              'name': '网站首页',
+              'icon': 'settings',
+              'children': [
+                {
+                  'name': '首页导航',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '一级轮播',
+                  'route': '' // 轮播图
+                },
+                {
+                  'name': '二级轮播',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '产品设置',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '公司展示',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '合作公司',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '首页新闻',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '地图导航',
+                  'route': '' // 跳转路径
+                },
+                {
+                  'name': '页脚导航',
+                  'route': '' // 跳转路径
+                }
+              ]
+            }
+          ]
+        },
+        {
+          'name': '网站新闻页设置',
+          'icon': 'settings',
+          'groups': [
+            {
+              'name': '网站新闻页设置',
+              'icon': 'settings',
+              'children': [
+                {
+                  'name': '网站新闻页设置',
+                  'route': '' // 跳转路径
+                }
+
+              ]
+            }
+          ]
+        },
+        {
+          'name': '文档设置',
+          'icon': 'settings',
+          'groups': [
+            {
+              'name': '文档设置',
+              'icon': 'settings',
+              'children': [
+                {
+                  'name': '文档设置',
+                  'route': '' // 跳转路径
+                }
+
+              ]
+            }
+          ]
+        },
+        {
+          'name': '关于我们设置',
+          'icon': 'settings',
+          'groups': [
+            {
+              'name': '关于我们设置',
+              'icon': 'settings',
+              'children': [
+                {
+                  'name': '关于我们设置',
+                  'route': '' // 跳转路径
+                }
+              ]
+            }
+          ]
+        },
+        {
+          'name': '实体服务器页面设置',
+          'icon': 'settings',
+          'groups': [
+            {
+              'name': '实体服务器页面设置',
+              'icon': 'settings',
+              'children': [
+                {
+                  'name': '实体服务器页面设置',
+                  'route': '' // 跳转路径
+                }
+
+              ]
+            }
+          ]
+        },
+        {
+          'name': '通知公告设置',
+          'icon': 'settings',
+          'groups': [
+            {
+              'name': '公告设置',
+              'icon': 'settings',
+              'children': [
+                {
+                  'name': '公告设置',
+                  'route': '' // 跳转路径
+                }
+
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
