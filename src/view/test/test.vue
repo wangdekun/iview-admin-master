bd53774d2191514958097628f903391d41bf3067
zhangjy <1576962234@qq.com>
2019-04-19 10:17:23 +0800

    v-html

--- src/view/test/test.vue
+++ src/view/test/test.vue
@@ -1,16 +1,22 @@
 <template>
-<div></div>
+<div v-html="content"></div>
 </template>
 <script>
 export default {
   created () {
     this.getreq()
   },
+  data () {
+    return {
+      content: ''
+    }
+  },
   methods: {
     getreq () {
       var url = '/documentLibrary/getrequirement.json'
       // var data = {}
       this.$get(url, { 'id': 4 }, response => {
+        this.content = response.data.content
         console.log(response.data.content, '*********', typeof (response.data.content))
       })
     }
